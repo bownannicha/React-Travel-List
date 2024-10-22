4310f7f48cb904a84512f6ff098f900121bfce01
buffy <nannicha.thunell@gmail.com>
2024-10-22 09:16:56 +0200

    added computed stats component

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -124,10 +124,25 @@ function Item({ item, onDeleteItem, onToggleItem }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list 🚀</em>
+      </p>
+    );
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
+
   return (
     <footer className="stats">
-      <em> You have X items on your list, and you already packed X (X%)</em>
+      <em>
+        {percentage === 100
+          ? "You've got everything! Ready to go ✈"
+          : `🧳 You have ${numItems} items on your list, and you already packed
+        ${numPacked} (${percentage}%)`}
+      </em>
     </footer>
   );
 }
